13b9a0c97113cac5888fbe6aaf253c9068018324
sigil-03 <sigil@glyphs.tech>
2025-03-07 10:49:24 -0700

    add innocent face + cli switching

---
+++ emote/src/emote.rs
@@ -0,0 +1,7 @@
+use clap::Subcommand;
+
+#[derive(Subcommand)]
+pub enum Emotes {
+    Blush,
+    Innocent,
+}
--- emote/src/main.rs
+++ emote/src/main.rs
@@ -1,5 +1,7 @@
 use std::thread;
 
+mod emote;
+
 use embedded_graphics::{
     image::{Image, ImageRawLE},
     mono_font::{ascii::FONT_10X20, MonoTextStyleBuilder},
@@ -17,7 +19,17 @@ use ssd1309::{mode::GraphicsMode, Builder};
 
 use display_interface_spi::SPIInterface;
 
+use clap::{command, Parser, Subcommand};
+
+#[derive(Parser)]
+struct Cli {
+    #[command(subcommand)]
+    emote: emote::Emotes,
+}
+
 fn main() {
+    let cli = Cli::parse();
+
     let gpio = Gpio::new().unwrap();
     // either 22 or 15 - i think 22
     let dc = gpio.get(25).unwrap().into_output();
@@ -27,31 +39,23 @@ fn main() {
     let spidev = SimpleHalSpiDevice::new(spi);
     let interface = SPIInterface::new(spidev, dc);
 
-    // let mut display = Ssd1306::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
-    //     .into_buffered_graphics_mode();
-
-    // let mut delay = thread::sleep(1);
-
     let mut disp: GraphicsMode<_> = Builder::new().connect(interface).into();
     // disp.reset(&mut reset &mut
     disp.init().unwrap();
     disp.flush().unwrap();
 
-    let im: ImageRawLE<BinaryColor> =
+    let blush: ImageRawLE<BinaryColor> =
         ImageRawLE::new(include_bytes!("../images/raw/blush.raw"), 128);
+
+    let innocent: ImageRawLE<BinaryColor> =
+        ImageRawLE::new(include_bytes!("../images/raw/innocent.raw"), 128);
+
+    let im = match cli.emote {
+        emote::Emotes::Blush => blush,
+        emote::Emotes::Innocent => innocent,
+    };
 
     Image::new(&im, Point::new(0, 0)).draw(&mut disp).unwrap();
 
-    // let text_style = MonoTextStyleBuilder::new()
-    //     .font(&FONT_10X20)
-    //     .text_color(BinaryColor::On)
-    //     .build();
-
-    // Text::with_baseline("(✿˶>﹏<˶)", Point::zero(), text_style, Baseline::Top)
-    //     .draw(&mut disp)
-    //     .unwrap();
-
     disp.flush().unwrap();
-
-    println!("Hello, world!");
 }
